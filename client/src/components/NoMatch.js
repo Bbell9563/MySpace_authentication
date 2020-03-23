f8b175f8bb1fa66f3ae4b76ec8dd8487904d2a17
Brianna Bell <bbell9563@gmail.com>
2020-03-23 14:04:17 -0600

    implemented router and stubbed out a NavBar

---
+++ client/src/components/NoMatch.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import {Header, Button} from 'semantic-ui-react'
+import { Link } from 'react-router-dom'
+
+const NoMatch = () => {
+  return(
+    <Header as='h3' textAlign='center'>
+      Page Not Found
+      <Link to='/'>
+        <Button>Home</Button>
+      </Link>
+    </Header>
+  )
+}
+
+export default NoMatch
