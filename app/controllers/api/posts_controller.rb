89e148b00fc72a0cdff7b63888184fdfa72a60b5
Scott Key <scottdkey@gmail.com>
2020-03-24 14:58:09 -0600

    posts should propagate - no current posts, working on way to make them now

--- app/controllers/api/posts_controller.rb
+++ app/controllers/api/posts_controller.rb
@@ -1,7 +1,13 @@
+# frozen_string_literal: true
+
 class Api::PostsController < ApplicationController
+  before_action :authenticate_user
   def index
+    render json: User.all_posts(current_user.liked_posts)
   end
 
   def update
+    current_user.liked_posts << params[:id].to_i
+    current_user.save
   end
 end
--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,18 +1,52 @@
 import React from 'react'
-import { Header, Card, Image} from 'semantic-ui-react'
+import { Header, Card, Image, Button, Icon} from 'semantic-ui-react'
 import { AuthConsumer } from '../providers/AuthProvider'
+import { Link } from 'react-router-dom'
+import Axios from 'axios'
 
 class Home extends React.Component {
+  state = { posts: []}
+
+  componentDidMount() {
+    Axios.get('api/posts').then(res => this.setState({ posts: res.data}))
+  }
+
+  posts = () => {
+    const { posts } = this.state
+    console.log(this.state.posts);
+    posts.map((p, index) =>(
+      <Card key={index}>
+        <Card.Content>
+          <Card.Header>{p.body}</Card.Header>
+        </Card.Content>
+        <Card.Meta>by: {this.props.nickname}</Card.Meta>
+        <Card.Content>Replies go here</Card.Content>
+      </Card>
+    ))
+    return posts
+      
+    }
+  
   render(){
   const { auth: { user } } = this.props
+  const posts = this.posts()
   return (
     <>
-      <Header as='h1' textAlign='center'>
+      <Header as="h1" textAlign="center">
         MyZone
-    </Header>
-        <Header as='h1'><Image size='mini' src={require('./images/photo.png')} />{user.nickname}</Header>
+      </Header>
+      <Header as="h1">
+        <Image size="mini" src={require("./images/photo.png")} />
+        {user.nickname}
+      </Header>
+      <div>
+        <br />
+        <Header as="h2">My Feed</Header>
+        <br />
+         {this.posts()}
+      </div>
     </>
-  )}
+  );}
 }
 
 const ConnectedHome = (props) => {
