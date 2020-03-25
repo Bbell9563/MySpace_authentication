4ac8398e5455d2ae8343a9c772bf378156891545
Scott Key <scottdkey@gmail.com>
2020-03-24 22:26:17 -0600

    trying to get things up and running

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -5,58 +5,72 @@ import { AuthConsumer } from '../providers/AuthProvider'
 import Axios from 'axios'
 
 class Home extends React.Component {
-  state = { posts: [] }
+  state = { posts: [], postCreator:{} }
 
   componentDidMount() {
     Axios.get('api/posts').then(res => this.setState({ posts: res.data })).catch(e=>console.log(e))
   }
 
+  getPostUser = (id)=> {
+    console.log(id)
+    Axios.get(`/api/posts/getUser${id}`)
+      .then(res => {
+        console.log(res);
+        // return res.data
+        // res.data.filter((user)=>{
+        // return (id === user.id)
+
+        // })
+        // console.log(postCreator)
+        // return postCreator;
+      })
+      .catch(e => console.log(e));
+    
+  }
+
   allPosts = () => {
+    console.log(this.getPostUser(1))
     const { posts } = this.state
     var postStuff = null
-    console.log(posts);
     if (posts.length > 0) {
-      posts.map((p, index) => {
-        return(
-            postStuff = <p>{p.body}</p>
-        )
-      })
-    }
-    else {
+      return postStuff = posts.map(p => (
+        <div key={p.id}>
+          {/* {console.log(p.user_id)} */}
+          {/* {console.log(this.getPostUser(p.user_id))} */}
+          {/* <Image src={this.getPostUser(p.user_id).image}/> */}
+
+          
+          <p>{p.body}</p>
+        </div>
+      ));} else {
       return(
        postStuff = <Header as='h3' textAlign='center'> No Post Exist Yet</Header>
       )
     }
-    return(postStuff)
+    return postStuff
   }
 
   render() {
     return (
       <>
         <Segment>
-          <Header as='h1'>Create A Post</Header>
-          <Header as='h3'>
+          <Header as="h1">Create A Post</Header>
+          <Header as="h3">
             <Image
-              size='mini'
+              size="mini"
               circular
-              src={require('./images/photo.png')}
+              src={this.props.auth.user.image}
               style={style.image}
             />
             <Input
-              placeholder='Write Your Post Here'
+              placeholder="Write Your Post Here"
               style={style.inputBox}
-            >
-            </Input>
+            ></Input>
           </Header>
         </Segment>
-        <Segment>
-        {this.allPosts()}
-        </Segment>
-
-
-
+        <Segment>{this.allPosts()}</Segment>
       </>
-    )
+    );
   }
 }
 
--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,10 @@
 
 Rails.application.routes.draw do
   namespace :api do
-    resources :posts, only: %i[index update]
+    resources :posts, only: %i[index update getUser]
     get 'my_posts', to: 'posts#my_posts'
   end
+
   mount_devise_token_auth_for 'User', at: 'api/auth'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
